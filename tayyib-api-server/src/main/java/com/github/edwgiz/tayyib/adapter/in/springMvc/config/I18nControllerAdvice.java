06ab0f23f4a6f18a15447a76dd92111dc441d6c8
Eduard Gizatullin <edwgiz@zohomail.eu>
2026-09-01 00:16:46 +0300

    Add Arabic i18n
    Signed-off-by: Eduard Gizatullin <edwgiz@zohomail.com>
    
    Signed-off-by: Eduard Gizatullin <edwgiz@zohomail.eu>

--- tayyib-api-server/src/main/java/com/github/edwgiz/tayyib/adapter/in/springMvc/config/I18nControllerAdvice.java
+++ tayyib-api-server/src/main/java/com/github/edwgiz/tayyib/adapter/in/springMvc/config/I18nControllerAdvice.java
@@ -1,14 +1,22 @@
 package com.github.edwgiz.tayyib.adapter.in.springMvc.config;
 
-import com.github.edwgiz.tayyib.domain.model.I18n;
 import com.github.edwgiz.tayyib.domain.usecase.i18n.GetBundleUsecase;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.util.Locale;
+import java.util.Set;
 
 @ControllerAdvice
 class I18nControllerAdvice {
+
+    private static final Set<String> RTL_LANGUAGES = Set.of(
+            "ar", // Arabic script
+            "fa", // Arabic script
+            "pa", // Arabic script
+            "ps", // Arabic script
+            "ur");// Nastaliq script
 
     private final GetBundleUsecase getBundleUsecase;
 
@@ -16,8 +24,14 @@ class I18nControllerAdvice {
         this.getBundleUsecase = getBundleUsecase;
     }
 
-    @ModelAttribute("i18n")
-    public I18n i18n(Locale locale) {
-        return getBundleUsecase.apply(locale);
+    @ModelAttribute
+    public void addI18nModelAttributes(final Model model, final Locale locale) {
+        final var i18n = getBundleUsecase.apply(locale);
+        model.addAttribute("i18n", i18n);
+        final var lang = locale.getLanguage();
+        model.addAttribute("lang", lang);
+        if (RTL_LANGUAGES.contains(lang)) {
+            model.addAttribute("dir", "rtl");
+        }
     }
 }
