a4287513b721368328ff7d805b4f6d19153b7dc3
Eduard Gizatullin <edwgiz@zohomail.eu>
2026-09-01 14:39:45 +0300

    Optimise web static resource compression
    Signed-off-by: Eduard Gizatullin <edwgiz@zohomail.eu>

--- tayyib-api-server/src/main/java/com/github/edwgiz/tayyib/adapter/in/springMvc/infra/WebMvcConfigurer.java
+++ tayyib-api-server/src/main/java/com/github/edwgiz/tayyib/adapter/in/springMvc/infra/WebMvcConfigurer.java
@@ -3,15 +3,19 @@ package com.github.edwgiz.tayyib.adapter.in.springMvc.infra;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.resource.EncodedResourceResolver;
 
+import java.util.List;
+
 
 public class WebMvcConfigurer implements org.springframework.web.servlet.config.annotation.WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        final var encodedResourceResolver = new EncodedResourceResolver();
+        encodedResourceResolver.setContentCodings(List.of("br")); // leave Brotli only
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:webapp/static")
                 .setCachePeriod(0)
                 .resourceChain(true)
-                .addResolver(new EncodedResourceResolver());
+                .addResolver(encodedResourceResolver);
     }
 }
